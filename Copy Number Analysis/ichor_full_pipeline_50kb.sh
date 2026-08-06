1f35a9a0a218195c05b56bd9437696845091e8e4
ImTom6 <260916819+ImTom6@users.noreply.github.com>
2026-08-06 10:18:55 +0100

    Update ichor_full_pipeline_50kb.sh

--- Copy Number Analysis/ichor_full_pipeline_50kb.sh
+++ Copy Number Analysis/ichor_full_pipeline_50kb.sh
@@ -62,7 +62,7 @@ file_name=${patient}_sorted.bam
 #2. Ichor Prep
 $hmm_util/readCounter --window 50000 \
 --quality 20 \
---chromosome "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY" $in_util/$file_name > $out_util/${patient}_500kb.wig
+--chromosome "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY" $in_util/$file_name > $out_util/${patient}_50kb.wig
 
 #3. Run Ichor
 Rscript $ichor/scripts/runIchorCNA.R --id $patient \
