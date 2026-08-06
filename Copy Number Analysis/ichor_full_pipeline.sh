c3aba0b3222b170158e46ec437d8e6457dd49250
ImTom6 <260916819+ImTom6@users.noreply.github.com>
2026-08-06 09:26:48 +0100

    Update ichor_full_pipeline.sh

--- Copy Number Analysis/ichor_full_pipeline.sh
+++ Copy Number Analysis/ichor_full_pipeline.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-#SBATCH --job-name=ichor_pipeline
+#SBATCH --job-name=ichor_pipeline_full_500kb
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=16:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=16gb
 #SBATCH --export=NONE
 
@@ -60,19 +60,22 @@ file_name=${patient}_sorted.bam
 
 
 #2. Ichor Prep
-$hmm_util/readCounter --window 1000000 --quality 20 --chromosome "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY" $in_util/$file_name > $out_util/${patient}.wig
-
+$hmm_util/readCounter --window 500000 \
+--quality 20 \
+--chromosome "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY" $in_util/$file_name > $out_util/${patient}_500kb.wig
 
 #3. Run Ichor
+
 Rscript $ichor/scripts/runIchorCNA.R --id $patient \
-  --WIG $in_ichor/${patient}.wig --ploidy "c(2,3)" --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 \
-  --gcWig $ichor/inst/extdata/gc_hg38_1000kb.wig \
-  --mapWig $ichor/inst/extdata/map_hg38_1000kb.wig \
+  --WIG $in_ichor/${patient}_500kb.wig --ploidy "c(2)" --normal "c(0.95, 0.99, 0.995, 0.999)" --maxCN 5 \
+  --gcWig $ichor/inst/extdata/gc_hg38_500kb.wig \
+  --mapWig $ichor/inst/extdata/map_hg38_500kb.wig \
   --centromere $ichor/inst/extdata/GRCh38.GCA_000001405.2_centromere_acen.txt \
-  --normalPanel $ichor/inst/extdata/HD_ULP_PoN_hg38_1Mb_median_normAutosome_median.rds \
-  --includeHOMD False --chrs "c(1:22, \"X\")" --chrTrain "c(1:22)" \
-  --estimateNormal True --estimatePloidy True --estimateScPrevalence True \
-  --scStates "c(1,3)" --txnE 0.9999 --txnStrength 10000 --outDir $out_ichor
+  --normalPanel $ichor/inst/extdata/HD_ULP_PoN_hg38_500kb_median_normAutosome_median.rds \
+  --includeHOMD False --chrs "c(1:22)" --chrTrain "c(1:22)" \
+  --estimateScPrevalence False \
+  --estimateNormal True --estimatePloidy True \
+  --txnE 0.999999 --txnStrength 10000 --outDir $out_ichor
   
 #4. Clean up files
 rm $in/${patient}_cat.bam
